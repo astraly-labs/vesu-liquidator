699647a58f0b79193df60a9824d0cbde96b62592
akhercha <akherchache@pm.me>
2024-08-20 00:47:28 +0200

    feat(liquidation_process): Commenting

--- src/main.rs
+++ src/main.rs
@@ -18,12 +18,12 @@ use crate::{
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let (positions_sender, position_receiver) = mpsc::channel::<Position>(1024);
-
     let rpc_url: Url = PUBLIC_MAINNET_RPC.parse()?;
     let pragma_api_key: String = env::var("PRAGMA_API_KEY")?;
     let apibara_key: String = env::var("APIBARA_API_KEY")?;
 
+    // Channel for positions communication between services
+    let (positions_sender, position_receiver) = mpsc::channel::<Position>(1024);
     let indexer_service = IndexerService::new(apibara_key, positions_sender);
     let monitoring_service = MonitoringService::new(rpc_url, pragma_api_key, position_receiver);
 
