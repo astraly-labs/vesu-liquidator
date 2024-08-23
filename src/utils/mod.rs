f99fd6f58dc4e762ca7f6d3860a7d0bf967c4add
akhercha <akherchache@pm.me>
2024-08-23 07:43:35 +0200

    fix(restore_stuff):

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -2,15 +2,14 @@ use bigdecimal::{num_bigint::BigInt, BigDecimal};
 
 pub mod conversions;
 
+/// Apply a small overhead of 2% to the provided number.
 pub fn apply_overhead(num: BigDecimal) -> BigDecimal {
-    // we apply overhead of 2% as in vesu frontend
     let overhead_to_apply = BigDecimal::new(BigInt::from(102), 2);
     num * overhead_to_apply
 }
 
 pub fn setup_tracing() {
     tracing_subscriber::fmt()
-        // enable everything
         .with_max_level(tracing::Level::INFO)
         .compact()
         .with_file(false)
